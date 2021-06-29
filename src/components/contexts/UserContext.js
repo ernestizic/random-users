99f84b7e534db446acefd443647c60cf2cde1b7c
ernestizic <ieifeanyichukwu@gmail.com>
2021-06-29 09:50:44 +0100

    pagination added

--- src/components/contexts/UserContext.js
+++ src/components/contexts/UserContext.js
@@ -8,14 +8,14 @@ const UserContextProvider = (props) => {
     const [gender, setGender] = useState('All Users')
     const [clonedUsers, setClonedUsers] = useState([])
     const [isLoading, setIsLoading] = useState(false);
-    //const [currentPage, setCurrentPage] = useState(1)
-    //const [usersPerPage, setUsersPerPage] = useState(3)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [usersPerPage] = useState(3)
 
 
     //Get current users
-    //const indexOfLastUser = currentPage * usersPerPage;
-    //const indexOfFirstUser = indexOfLastUser - usersPerPage;
-    //const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser)
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = clonedUsers.slice(indexOfFirstUser, indexOfLastUser);
 
     useEffect(() => {
         fetchUsers();
@@ -28,7 +28,7 @@ const UserContextProvider = (props) => {
     const fetchUsers = async () => {
         try{
             setIsLoading(true);
-            const URL = `https://randomuser.me/api/?results=3`
+            const URL = `https://randomuser.me/api/?results=100`
             const res = await axios.get(URL);
             setUsers(res.data.results);
             setIsLoading(false);
@@ -38,31 +38,7 @@ const UserContextProvider = (props) => {
         };
     }
 
-    /*
-    const fetchUsers = async (gender)=> {
-        let URL;
-        if (gender) {
-            URL = `https://randomuser.me/api/?page=3&gender=${gender}&results=100&seed=foobar`
-        } else {
-            URL = 'https://randomuser.me/api/?page=3&results=100&seed=foobar'
-        }
-        setIsLoading(true)
-        const res = await axios.get(URL)
-        //console.log(res.data.results)
-        setUsers(res.data.results);
-        setIsLoading(false)
-    } */
-    
-    /******** a test by my Oga, Nonso.js ********/
-    /*
-    const fetchUsers = async (gender) => {
-        setIsLoading(true);
-        const url = `https://randomuser.me/api/${gender ?  `?gender=${gender}`: ""}`;
-        const res = await axios.get(url);
-        setUsers(res.data.results);
-        setIsLoading(false);
-      };
-      */
+
      const getQuery =(q)=> {
         setClonedUsers(users.filter(user => user.name.first.toLowerCase().includes(q.toLowerCase())))
      }
@@ -84,9 +60,20 @@ const UserContextProvider = (props) => {
         setClonedUsers(users.filter(user => user.gender ==='female'))
         setGender('Female Users')
     };
+    //Change page
+    const next =()=> {
+        setCurrentPage(currentPage + 1)
+    }
+    const prev =()=> {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+        }else{
+            setCurrentPage(1)
+        }
+    }
 
     return ( 
-        <UserContext.Provider value={{users, isLoading, clonedUsers, gender, allUsers, maleUsers, femaleUsers, getQuery}}>
+        <UserContext.Provider value={{users, isLoading, currentUsers, gender, allUsers, maleUsers, femaleUsers, getQuery, prev, next}}>
             {props.children}
         </UserContext.Provider>
      );
--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -2,19 +2,21 @@ import React, {useContext} from 'react';
 import {UserContext} from '../contexts/UserContext';
 import User from './User';
 import { IoIosCloudDownload } from 'react-icons/io';
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const UsersList = () => {
-    const {clonedUsers, isLoading} = useContext(UserContext)
+    const {currentUsers, isLoading, prev, next} = useContext(UserContext)
 
     let allUsers = isLoading ? (
         <div className='loading'>
             <p>Loading Users...</p>
         </div>
     ) : (
-        clonedUsers.length === 0 ? (
+        currentUsers.length === 0 ? (
             <p style={{textAlign: 'center'}}>No user to show</p>
         ) : (
-            clonedUsers.map(user => (
+            currentUsers.map(user => (
                 <User key={user.login.uuid} user={user}/>
             ))
         )
@@ -24,7 +26,11 @@ const UsersList = () => {
         <div>
            {allUsers}
            <div className='download-div'>
-                <button><IoIosCloudDownload style={{paddingRight: '12px'}}/> Download results</button>
+                <button className='download-btn'><IoIosCloudDownload style={{paddingRight: '12px'}}/> Download results</button>
+                <div className='pagination'>
+                    <button className='fir' onClick={prev}><IoIosArrowBack/></button>
+                    <button className='sec' onClick={next}><IoIosArrowForward/></button>
+                </div>
             </div>
         </div>
      );
